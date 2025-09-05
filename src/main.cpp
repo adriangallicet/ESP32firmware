71ea36a32fd873b0488deebc0236785957bab8fb
Adrian Gallicet <adrigallicet@gmail.com>
2025-09-05 17:20:55 -0300

    Inclusion de WiFiManager al proyecto y prueba satisfactoria en la placa

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,27 @@
 #include <ArduinoJson.h> //manejo de JSON
 #include <PubSubClient.h> //manejo de MQTT
 #include <HTTPClient.h> //incluida por defecto en el core de la placa
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager librería para config. de la red, evitamos hardcoding de credenciales SSID y PASS
 
 // put function declarations here:
+void WIFI_ini();
+const char* mqtt_server = "192.168.1.20";
+
+//PINS
+#define led 2
+
+//
 
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
+
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    pinMode(led, OUTPUT);
+
+    WIFI_ini();
+    
+
 }
 
 void loop() {
@@ -18,3 +32,34 @@ void loop() {
 }
 
 // put function definitions here:
+
+void WIFI_ini(){
+  WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
+    // it is a good practice to make sure your code sets wifi mode how you want it.
+
+    //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wm;
+
+    // reset settings - wipe stored credentials for testing
+    // these are stored by the esp library
+    wm.resetSettings();
+
+    // Automatically connect using saved credentials,
+    // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
+    // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
+    // then goes into a blocking loop awaiting configuration and will return success result
+
+    bool res;
+    // res = wm.autoConnect(); // auto generated AP name from chipid
+    // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
+    res = wm.autoConnect("AutoConnectAP","password"); // password protected ap
+
+    if(!res) {
+        Serial.println("Failed to connect");
+        // ESP.restart();
+    } 
+    else {
+        //if you get here you have connected to the WiFi    
+        Serial.println("connected...yeey :)");
+    }
+}
