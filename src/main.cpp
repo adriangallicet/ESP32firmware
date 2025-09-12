7ed83b244052e59f3c78279b90adbea43903fc8f
Adrian Gallicet <adrigallicet@gmail.com>
2025-09-12 18:13:13 -0300

    primera conn con broker desde dispositivo exitosa

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,25 @@
 #include <Arduino.h>
 #include "IoTicosSplitter.h" //libreria para separar strings//lo usaremos para separar topicos MQTT
-#include <ArduinoJson.h> //manejo de JSON
+#include <ArduinoJson.h> //manejo de JSON y serializacion para envio y recepcion de datos arduinojson.org
 #include <PubSubClient.h> //manejo de MQTT
 #include <HTTPClient.h> //incluida por defecto en el core de la placa
 #include <WiFiManager.h> // https://github.com/tzapu/WiFiManager librería para config. de la red, evitamos hardcoding de credenciales SSID y PASS
+#include <WiFi.h>
 
 // put function declarations here:
 void WIFI_ini();
+void check_mqttConn();
+bool reconnect();
 const char* mqtt_server = "192.168.1.20";
+const char * broker_user = "dev";
+const char * broker_pass = "";
+
+WiFiClient espclient; //instancia del cliente de red
+PubSubClient client(espclient); //declaracion del cliente MQTT e indicamos la conexion
+long lastReconnAtt = 0;
+
+
+JsonDocument mqtt_data_doc; //JsonDocument palabra reservada por la libreria ArduinoJson// reserva espacio en memoria
 
 //PINS
 #define led 2
@@ -23,15 +35,38 @@ void setup() {
 
     WIFI_ini();
     
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  check_mqttConn();
 
 }
 
 // put function definitions here:
+
+bool reconnect(){
+  //Set Mqtt sv
+  client.setServer(mqtt_server, 1883);
+  Serial.print("\n\n\nIntentando conexion Mqtt");
+
+  
+  //const char *str_client_id = client_id.c_str();
+  String topic = "683a1f9413ca368f47a7b655/687eac95e37a35affe53f0ea/+/actdata";
+  String client_id = String("device_") + random(1, 9999);
+  
+  if(client.connect(client_id.c_str(), broker_user, broker_pass)){
+    Serial.print("\n\n\nCliente Mqtt conectado");
+    delay(2000); //delay para poder ver el mensaje en monitor serial, necesario?
+    client.subscribe(topic.c_str()); //el metodo espera un char array
+
+    return true;
+  }else{
+    Serial.print("\n\n\nFallo conexion Mqtt");
+    return false;
+  }
+
+}
 
 void WIFI_ini(){
   WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
@@ -42,7 +77,7 @@ void WIFI_ini(){
 
     // reset settings - wipe stored credentials for testing
     // these are stored by the esp library
-    wm.resetSettings();
+   // wm.resetSettings();
 
     // Automatically connect using saved credentials,
     // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
@@ -62,4 +97,20 @@ void WIFI_ini(){
         //if you get here you have connected to the WiFi    
         Serial.println("connected...yeey :)");
     }
+}
+
+void check_mqttConn(){
+  if(!client.connected()){
+    long now = millis(); // tiempo en ms desde que inicio la placa
+    if(now - lastReconnAtt > 5000){ //logica para evitar codigo bloqueante con los delay()
+      lastReconnAtt = millis();
+      if(reconnect()){
+        lastReconnAtt = 0;
+      }
+
+    }
+
+  }else{
+    client.loop(); //metodo para procesar los mensajes mqtt llegados al buffer
+  }
 }
